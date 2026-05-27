6336b07d12fc28e5289ec4fa6fa3d6e0740db280
Utsav786-2024 <utsavkumartharu07@gmail.com>
2026-05-27 23:01:42 +0545

    MY first JAVA programming code

---
+++ Swaptwovalues.java
@@ -0,0 +1,17 @@
+public class Swaptwovalues {
+    public static void main(String [] avgs){
+
+        int a=10;
+
+        int b=20;
+
+         b = b - a;
+
+         a = b + a;
+        
+         System.out.println("value of a="+a);
+         System.out.println("value of b="+b);
+
+     }
+    
+}
